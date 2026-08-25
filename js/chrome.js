3f15bc1bd5f9e9fc353a3d972d2ee9d06194275a
Bryan Mendez <bmendez@admira.com>
2026-08-25 13:39:06 -0600

    fix:cambio de logo

--- js/chrome.js
+++ js/chrome.js
@@ -1,7 +1,9 @@
 function renderChrome(showTopLabel) {
   const label = showTopLabel ? '<div class="top-label">Lenovo LAB</div>' : "";
   return `
-    <div class="side-tab"><span>Lenovo</span></div>
+    <div class="side-tab">
+      <img src="assets/img/logo.png" alt="Lenovo" class="side-tab-logo" />
+    </div>
     ${label}
   `;
 }
